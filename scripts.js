dba2d2bc475e9e39136c4c7ef4271f8e45bea09b
Ryan Carlson <americanized@gmail.com>
2021-04-17 12:24:03 -0700

    code cleanup

--- scripts.js
+++ scripts.js
@@ -2,27 +2,6 @@ let str = prompt('Type Hello World')
 
 for (let i = 0; i < str.length; i++) {
   let n = str.charCodeAt(i);
-//   document.getElementById("demo").innerHTML = n;
-//   console.log(str[i]);
     console.log(n);
-    document.write(`${n},`);
+    document.write(`${n}`);
 }
-
-
-// SAFEKEEPING
-
-// let str = prompt('Type Hello World')
-
-// function myFunction() {
-
-//   let n = str.charCodeAt(0);
-//   document.getElementById("demo").innerHTML = n;
-// }
-
-// for (let i = 0; i < str.length; i++) {
-//   console.log(str[i]);
-// }
-
-
-
-
