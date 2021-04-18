c323d9ac80320482557e653a7102b7c1b0ecbebd
Ryan Carlson <americanized@gmail.com>
2021-04-17 21:41:04 -0700

    returns unicode to same page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
-let str = prompt('Type Hello World')
 
-for (let i = 0; i < str.length; i++) {
-  let n = str.charCodeAt(i);
-    console.log(n);
-    document.write(`${n}`);
+
+// prompt user for string, transform string into unicode.
+let secretRaw = prompt('Insert secret message here.')
+let info = 'This is your secret unicode: ';
+const para = document.querySelector('p');
+
+
+//function allows the transformation to happen via a button.
+function toUnicode() {
+  for (let i = 0; i < secretRaw.length; i++) {
+    let n = secretRaw.charCodeAt(i);
+    info += n + ', ';
+    para.textContent = info;
+
+  }
 }
+
+//Guess what? It doesn't work... it merely returns line 5
+console.log(info);
+
+
+// let secretNum = new Array();
+// document.getElementById("demo").innerHTML = cars;
+
+
+
+
+
